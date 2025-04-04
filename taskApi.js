649b90b2720e89c1782f11353af601070b4ea8c8
John Adaji Sunday <69393137+gionsunday@users.noreply.github.com>
2025-04-04 18:26:03 +0100

    Update taskApi.js

--- taskApi.js
+++ taskApi.js
@@ -31,13 +31,13 @@ app.use('/api/v01/user', user)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 const start = async () =>{
     try {
 
         await connectDB(process.env.TASKDB_URL)
-         app.listen(5000, 
-                console.log('app is listenig at port 5000')
+         app.listen(port, 
+                console.log(`app is listenig at port ${port}`)
 )
     } catch (error) {
         console.log(error)
